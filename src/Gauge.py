77c6d2f74549bd3fbcac0c5501638ac05c4dfaa0
stergios zissakis <stergios.zissakis@gmail.com>
2022-09-20 16:33:15 +0100

    Added more gauges. TODO the frills

--- src/Gauge.py
+++ src/Gauge.py
@@ -11,23 +11,29 @@ class Gauge:
         self.obdCommand = command
         self.frequency = freq
 
+    def getValue(self, reading):
+        return int(reading.value.magnitude)
+
     def processReading(self, reading):
-        val = reading.value.magnitude
-        #self.gaugeUI.value = val
+        val = self.getValue(reading)
         self.gaugeUI.update_value(val)
 
     def toJob(self):
         return Job(self)
+
+    def applyGaugeCommonAttributes(self):
+        self.gaugeUI.set_enable_ScaleText(True)
+        self.gaugeUI.set_enable_value_text(True)
+        self.gaugeUI.setMouseTracking(False)
 
 class GaugeRPM(Gauge):
 
     def __init__(self):
         super(GaugeRPM, self).__init__(obd.commands.RPM, 100)
         self.gaugeUI = AnalogGaugeWidget()
-        self.gaugeUI.set_MaxValue(6000)
-        self.gaugeUI.set_enable_ScaleText(True)
-        self.gaugeUI.set_enable_value_text(True)
-        self.gaugeUI.setMouseTracking(False)
+        self.gaugeUI.set_MinValue(0)
+        self.gaugeUI.set_MaxValue(9000)
+        self.applyGaugeCommonAttributes()
 
 
 class GaugeSpeed(Gauge):
@@ -35,7 +41,58 @@ class GaugeSpeed(Gauge):
     def __init__(self):
         super(GaugeSpeed, self).__init__(obd.commands.SPEED, 100)
         self.gaugeUI = AnalogGaugeWidget()
+        self.gaugeUI.set_MinValue(0)
         self.gaugeUI.set_MaxValue(280)
-        self.gaugeUI.set_enable_ScaleText(True)
-        self.gaugeUI.set_enable_value_text(True)
-        self.gaugeUI.setMouseTracking(False)
+        self.applyGaugeCommonAttributes()
+
+class GaugeOilTemp(Gauge):
+
+    def __init__(self):
+        super(GaugeOilTemp, self).__init__(obd.commands.OIL_TEMP, 500)
+        self.gaugeUI = AnalogGaugeWidget()
+        self.gaugeUI.set_MinValue(0)
+        self.gaugeUI.set_MaxValue(140)
+        self.applyGaugeCommonAttributes()
+
+class GaugeCoolantTemp(Gauge):
+
+    def __init__(self):
+        super(GaugeCoolantTemp, self).__init__(obd.commands.COOLANT_TEMP, 500)
+        self.gaugeUI = AnalogGaugeWidget()
+        self.gaugeUI.set_MinValue(0)
+        self.gaugeUI.set_MaxValue(140)
+        self.applyGaugeCommonAttributes()
+
+class GaugeIntakePressure(Gauge):
+
+    def __init__(self):
+        super(GaugeIntakePressure, self).__init__(obd.commands.INTAKE_PRESSURE, 300)
+        self.gaugeUI = AnalogGaugeWidget()
+        self.gaugeUI.set_MaxValue(0)
+        self.gaugeUI.set_MaxValue(300)
+        self.applyGaugeCommonAttributes()
+
+class GaugeIntakeTemp(Gauge):
+
+    def __init__(self):
+        super(GaugeIntakeTemp, self).__init__(obd.commands.INTAKE_TEMP, 300)
+        self.gaugeUI = AnalogGaugeWidget()
+        self.gaugeUI.set_MaxValue(0)
+        self.gaugeUI.set_MaxValue(140)
+        self.applyGaugeCommonAttributes()
+
+
+
+class SolidGauge(Gauge):
+
+    def __init__(self, command, freq):
+        super(SolidGauge, self).__init__(command, freq)
+
+    def processReading(self, reading):
+        val = self.getValue(reading)
+        self.gaugeUI.value = val
+
+    def applyGaugeCommonAttributes(self):
+        pass
+
+
--- src/main.py
+++ src/main.py
@@ -23,11 +23,19 @@ if __name__ == "__main__":
 	
 	rpmGauge = GaugeRPM()
 	klmGauge = GaugeSpeed()
+	intakePressureGauge = GaugeIntakePressure()
+	intakeTempGauge = GaugeIntakeTemp()
+	oilTempGauge = GaugeOilTemp()
+	coolantTempGauge = GaugeCoolantTemp()
 	
 	grid = QGridLayout(w)
 
-	grid.addWidget(klmGauge.gaugeUI, 0, 0)
-	grid.addWidget(rpmGauge.gaugeUI, 0, 1)
+	grid.addWidget(rpmGauge.gaugeUI, 0, 0)
+	grid.addWidget(klmGauge.gaugeUI, 0, 1)
+	grid.addWidget(oilTempGauge.gaugeUI, 0, 2)
+	grid.addWidget(intakePressureGauge.gaugeUI, 1, 0)
+	grid.addWidget(intakeTempGauge.gaugeUI, 1, 1)
+	grid.addWidget(coolantTempGauge.gaugeUI, 1, 2)
 	
 	while not obdConnection.isConnected():
 		syslog.syslog(syslog.LOG_ERR, 'Main: OBD Connector not established for car. Retring')
@@ -43,8 +51,21 @@ if __name__ == "__main__":
 	
 	rpmJob = rpmGauge.toJob()
 	jobManager.watch(rpmGauge.frequency, rpmJob, obdDispatcher)  
+
 	klmJob = klmGauge.toJob()
 	jobManager.watch(klmGauge.frequency, klmJob, obdDispatcher)  
+
+	oilTempJob = oilTempGauge.toJob()
+	jobManager.watch(oilTempGauge.frequency, oilTempJob, obdDispatcher)  
+
+	intakePressureJob = intakePressureGauge.toJob()
+	jobManager.watch(intakePressureGauge.frequency, intakePressureJob, obdDispatcher)  
+
+	intakeTempJob = intakeTempGauge.toJob()
+	jobManager.watch(intakeTempGauge.frequency, intakeTempJob, obdDispatcher)  
+
+	coolantTempJob = coolantTempGauge.toJob()
+	jobManager.watch(coolantTempGauge.frequency, coolantTempJob, obdDispatcher)  
 
 	w.show()
 	sys.exit(app.exec_())
